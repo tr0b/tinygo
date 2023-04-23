6dc9bc81d7f6f4f4a9d22df801b4fea66768130c
tr0b <60306480+tr0b@users.noreply.github.com>
2023-04-23 17:13:00 -0600

    Will add initial go map example program, based on Grider's Go Course

---
+++ map/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
+
+	printMap(colors)
+
+}
+
+func printMap(c map[string]string) {
+	for key, value := range c {
+		fmt.Println("Hex code for", key, "is", value)
+	}
+}
